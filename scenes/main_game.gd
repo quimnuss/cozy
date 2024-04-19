323556fbbea332f2f23e9d2e481b5ece74679967
Quim Nuss Székely <quimnuss@altaveu>
2024-04-19 22:58:55 +0200

    new: spore, win_scene, goal reactions

--- scenes/main_game.gd
+++ scenes/main_game.gd
@@ -1,17 +1,34 @@
 extends Node2D
 @onready var player = $Player
-@onready var water_level : ProgressBar = $UI/CenterContainer/GridContainer/WaterProgressBar
-@onready var light_level : ProgressBar = $UI/CenterContainer/GridContainer/LightProgressBar
+@onready var water_level : ProgressBar = %WaterProgressBar
+@onready var light_level : ProgressBar = %LightProgressBar
 @onready var start_position = $StartPosition
 @onready var trail_man = $TrailMan
+@onready var win_scene = $WinScene
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
     #player.water_changed.connect(water_level.set_value_no_signal)
     #player.light_changed.connect(light_level.set_value_no_signal)
-    pass
 
+    var goals : Array = get_tree().get_nodes_in_group('goals')
+    for goal : Goal in goals:
+        goal.goal_reached.connect(_on_goal_reached)
+
+    #var done = await get_tree().create_timer(1).timeout
+    #win_scene.play_win()
+
+func _on_goal_reached(goal_num : int):
+    match goal_num:
+        0:
+            player.goal0()
+        1:
+            player.goal1()
+        2:
+            player.goal2()
+        3:
+            player.goal3()
 
 func _process(delta):
     water_level.set_value_no_signal(player.water_level)
@@ -24,3 +41,9 @@ func respawn():
 
 func _on_player_death():
     respawn()
+
+func show_win():
+    win_scene.play_win()
+
+func _on_goal_3_goal_reached(goal_num):
+    show_win()
---
+++ scripts/status_bar.gd
@@ -0,0 +1,27 @@
+extends ProgressBar
+
+const BLINK_THRESHOLD : float = 0.5
+var blink_rate : float = 1.0
+@export var blink_curve : Curve
+
+var elapsed = 0
+
+var base_color : Color
+
+const HIGH_COLOR : Color = Color('#8C3211')
+
+func _ready():
+    base_color = self.modulate
+
+
+func _process(delta):
+    elapsed += delta
+    if ratio < BLINK_THRESHOLD:
+        var blink_rate = max(ratio/BLINK_THRESHOLD,0.2)
+        elapsed = fmod(elapsed,blink_rate)
+        var white_ratio : float = blink_curve.sample(elapsed/blink_rate)
+        self.modulate = HIGH_COLOR*white_ratio + (1-white_ratio)*self.base_color
+    else:
+        if self.modulate != base_color:
+            self.modulate = base_color
+
