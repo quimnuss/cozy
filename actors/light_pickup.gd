3fe662d8d64cac40f840ec9a4123dcbdfe36fd8d
Quim Nuss Székely <quimnuss@altaveu>
2024-04-20 11:17:25 +0200

    change water and light logic, light is state

--- actors/light_pickup.gd
+++ actors/light_pickup.gd
@@ -4,20 +4,52 @@ extends Node2D
 
 var is_pickable : bool = true
 
-func _ready():
-    animated_sprite_2d.play('default')
+var invader : Player
 
-func picked_up(player):
+@onready var light_collision_shape_2d : CollisionShape2D = $LightArea2D/CollisionShape2D
+@onready var burn_collision_shape_2d = $BurnArea2D/CollisionShape2D
+
+var light_range_max : float
+var light_range_min : float
+
+func _ready():
+    light_range_max = light_collision_shape_2d.shape.radius
+    light_range_min = burn_collision_shape_2d.shape.radius
+
+func picked_up(player : Player):
     player.light()
     animated_sprite_2d.play('dissolve')
     point_light_2d.visible = false
     is_pickable = false
 
-func _on_area_2d_body_entered(body):
-    if body is Player and is_pickable:
-        picked_up(body)
+func _physics_process(delta):
+    if invader:
+        var invader_distance : float = self.global_position.distance_to(invader.global_position)
+        var ratio = clamp((invader_distance - light_range_min)/(light_range_max - light_range_min),0,1)
+        invader.light(ratio)
 
 
-func _on_animated_sprite_2d_animation_finished():
-    if animated_sprite_2d.animation == 'dissolve':
-        queue_free()
+
+
+func _on_light_area_2d_body_entered(body):
+    if body is Player:
+        animated_sprite_2d.play('light')
+        point_light_2d.modulate.a = 0.6
+        invader = body as Player
+
+
+func _on_light_area_2d_body_exited(body):
+    animated_sprite_2d.play('default')
+    point_light_2d.modulate.a = 1
+    invader.light(0)
+    invader = null
+
+
+func _on_burn_area_2d_body_entered(body):
+    if body is Player:
+        body.set_burn(true)
+
+
+func _on_burn_area_2d_body_exited(body):
+    if body is Player:
+        body.set_burn(false)
--- actors/player.gd
+++ actors/player.gd
@@ -3,9 +3,10 @@ extends CharacterBody2D
 class_name Player
 
 const SPEED = 200.0
-const JUMP_VELOCITY = -400.0
 
 @export var min_direction : Vector2 = Vector2(1, -0.1)
+@onready var animated_sprite_2d = $AnimatedSprite2D
+@onready var fire_particles = $FireParticles
 
 var water_level : float = 100
 var max_water_level : float = 100
@@ -13,10 +14,13 @@ var light_level : float = 100
 var max_light_level : float = 100
 
 var water_delta : float = 25
-var light_delta : float = 25
+var light_delta : float = 50
+const BURN_DELTA : float = 60
 
-var water_degrowth_rate : float = 10
-var light_degrowth_rate : float = 3
+var water_degrowth_rate : float = 3
+var base_light_degrowth_rate : float = 10
+var lit : float = 0
+var current_light_growth_rate : float
 
 var velocity_degrowth_impact : float = 10
 
@@ -32,29 +36,50 @@ signal death
 
 var can_move : bool = true
 
+var is_burning : bool = false
+
+var current_water_delta : float
+
 func _ready():
     Global.player = self
+    animated_sprite_2d.play('default')
 
 func water():
+    current_water_delta = water_delta
     water_level = clamp(water_level+water_delta,0,max_water_level)
     is_watered.emit()
     water_changed.emit(water_level/max_water_level)
 
-func light():
-    light_level = clamp(light_level+light_delta,0,max_light_level)
-    is_lit.emit()
-    light_changed.emit(light_level/max_light_level)
+func light(ratio = 1):
+    lit = light_delta*ratio
+    #light_level = clamp(current_light_delta,0,max_light_level)
+    #is_lit.emit()
+    #light_changed.emit(light_level/max_light_level)
+
+func set_burn(new_is_burning : bool):
+    is_burning = new_is_burning
+    if is_burning:
+        fire_particles.emitting = true
+        fire_particles.restart()
+    else:
+        fire_particles.emitting = false
+
 
 func respawn():
     light_level = max_light_level
     water_level = max_water_level
+    set_burn(false)
 
 func _process(delta):
 
-    var velocity_light_degrowth_rate : float = light_degrowth_rate + velocity.length()*velocity_degrowth_impact
+    var actual_burn_impact = BURN_DELTA if is_burning else 0
+    var velocity_water_degrowth_rate : float = water_degrowth_rate + (velocity.length()/SPEED)*velocity_degrowth_impact + actual_burn_impact
 
-    water_level = clamp(water_level - water_degrowth_rate*delta, 0, max_water_level)
-    light_level = clamp(light_level - light_degrowth_rate*delta, 0, max_light_level)
+    current_light_growth_rate = -base_light_degrowth_rate + lit
+
+    water_level = clamp(water_level - velocity_water_degrowth_rate*delta, 0, max_water_level)
+    light_level = clamp(light_level + current_light_growth_rate*delta, 0, max_light_level)
+
     water_changed.emit(water_level/max_water_level)
     light_changed.emit(light_level/max_light_level)
 
